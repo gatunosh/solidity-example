412c190f147de56ab0ea9bc4cebcd86d011d8481
Adrian Lima <adrigato3@gmail.com>
2024-04-22 20:07:51 -0500

    [ADD] Library to return the public wallet address to transfer ethers

---
+++ contracts/library.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+library WalletRepository {
+    function getWithdrawalWallet() external pure returns (address) {
+        return 0x703d06C760e4DD22a58Cf229767b73D69C4F108F; // Dirección de ejemplo
+    }
+}
