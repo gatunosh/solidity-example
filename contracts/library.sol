b42879b797bf307aa54c227f58a93718cd563b9f
Adrian Lima <57717754+gatunosh@users.noreply.github.com>
2024-04-22 21:01:15 -0500

    Update library.sol

--- contracts/library.sol
+++ contracts/library.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.4;
 
 library WalletRepository {
     function getWithdrawalWallet() external pure returns (address) {
-        return 0x703d06C760e4DD22a58Cf229767b73D69C4F108F; // Dirección de ejemplo
+        return 0x51734EAc7578030b560A7215466154bE2a1Dc9C1; // Dirección de ejemplo
     }
 }
